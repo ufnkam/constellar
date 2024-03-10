a902380cdb84c07f3dcca42e85a6598da3715911
Kamil Ufnal <ku@aiclearing.com>
2024-03-10 13:35:15 +0100

    feature: working grpc service

---
+++ constellar-core/src/main.rs
@@ -0,0 +1,9 @@
+use constellar_core::server::server::run_server;
+use tokio;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let _ = run_server().await;
+    Ok(())
+}
+
---
+++ constellar-core/src/server/server.rs
@@ -0,0 +1,34 @@
+pub mod ccore {
+    tonic::include_proto!("ccore");
+}
+
+use ccore::{HealthCheckResponse, HealthCheckRequest};
+use ccore::c_core_server::{CCore, CCoreServer};
+use tonic::transport::Server;
+use tonic::{Request, Response, Status};
+use std::env;
+
+#[derive(Debug, Default)]
+pub struct CCoreService {}
+
+#[tonic::async_trait]
+impl CCore for CCoreService {
+    async fn health_check(&self, request: Request<HealthCheckRequest>) -> Result<Response<HealthCheckResponse>, Status> {
+        print!("Health check requested.");
+        let reply = HealthCheckResponse{living: true};
+        Ok(Response::new(reply))
+    } 
+}
+
+pub async fn run_server() -> Result<(), Box<dyn std::error::Error>> {
+    let port = env::var("PORT").unwrap_or("50051".to_string());
+    let addr = format!("0.0.0.0:{}", port).parse()?;
+    let ccore = CCoreService::default();
+    let ccore = CCoreServer::new(ccore);
+    println!("Running on port {}...", port);
+    Server::builder()
+        .add_service(ccore)
+        .serve(addr)
+        .await?;
+    Ok(())
+}
